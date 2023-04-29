7a229ea888dac3669b00490da144fc54603ac41f
HitomiAMBE <hitomiambe@gmail.com>
2023-04-30 08:33:31 +0900

    Initial Commit

---
+++ Renda/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Renda
+//
+//  Created by 安部瞳 on 2023/04/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var countLabel: UILabel!
+    
+    @IBOutlet var tapButton: UIButton!
+    
+    var tapCount = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        tapButton.layer.cornerRadius = 125
+    }
+    
+    @IBAction func tapTapButton() {
+        
+        tapCount = tapCount + 1
+        countLabel.text = String(tapCount)
+        
+    }
+
+
+}
+
